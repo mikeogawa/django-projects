d8c090fd703d2a38f26eaa8540d6e5ed301905e0
mikeogawa <mike.ogawa@techmo.jp>
2021-08-29 16:06:33 +0900

    added return to clean

--- 01-clean_architecture_example/api/settings.py
+++ 01-clean_architecture_example/api/settings.py
@@ -114,6 +114,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
